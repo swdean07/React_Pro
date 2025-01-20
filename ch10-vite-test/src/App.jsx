5a8810c86313311219f1a159d80785eefbcaf349
swdean07 <swdean07@gmail.com>
2025-01-20 17:31:23 +0900

    ch10 일정관리 애플리케이션

---
+++ ch10-vite-test/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='react'>ch10 일정관리 애플리케이션</h1>
+      <div>Todo App을 만들자!</div>
+    </>
+  )
+}
+
+export default App
